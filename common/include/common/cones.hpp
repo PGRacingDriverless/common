ecea2ccf9b96abe2ad889cf58940a6c2f94b0dbe
Cyprian Kiedrowicz <cypkied@gmail.com>
2024-11-25 15:31:51 +0100

    przerobić cały kod w common, żeby był tylko w .hpp

--- common/include/common/cones.hpp
+++ common/include/common/cones.hpp
@@ -4,8 +4,38 @@
 #include "cones/cone_array.hpp"
 #include "cones/cone_pair_array.hpp"
 
-bool check_pairs_equality(const ConePair &pair1, const ConePair &pair2);
-void separate_cone_sides(const std::vector<Cone>& input_cone_array, ConeArray& inner_cone_array, ConeArray& outer_cone_array);
-void separate_cone_sides_from_cone_pairs(ConePairArray &cone_pair_array, ConeArray& inner_cone_array, ConeArray& outer_cone_array);
+static void separate_cone_sides(const std::vector<Cone>& input_cone_array, ConeArray& inner_cone_array, ConeArray& outer_cone_array){
+    for(auto& item: input_cone_array){
+        if(item.get_side() == Cone::TrackSide::OUTER){
+            outer_cone_array.add_cone(item);
+        } 
+        else if(item.get_side() == Cone::TrackSide::INNER) {
+            inner_cone_array.add_cone(item);
+        }
+    }
+}
+
+static void separate_cone_sides_from_cone_pairs(ConePairArray &cone_pair_array, ConeArray& inner_cone_array, ConeArray& outer_cone_array){
+    for(auto& item: cone_pair_array.get_pairs()){
+        inner_cone_array.add_cone(item.cone_inner);
+        outer_cone_array.add_cone(item.cone_outer);
+    }
+}
+
+static bool check_pairs_equality(const ConePair &pair1, const ConePair &pair2)
+{
+    bool inner_cone_equality = false;
+    bool outer_cone_equality = false;
+
+    if((pair1.cone_inner.get_x() == pair2.cone_inner.get_x()) && (pair1.cone_inner.get_y() == pair2.cone_inner.get_y())){
+        inner_cone_equality = true;
+    }
+
+    if((pair1.cone_outer.get_x() == pair2.cone_outer.get_x()) && (pair1.cone_outer.get_y() == pair2.cone_outer.get_y())){
+        outer_cone_equality = true;
+    }
+
+    return inner_cone_equality && outer_cone_equality;
+}
 
 #endif
--- common/include/common/math.hpp
+++ common/include/common/math.hpp
@@ -7,7 +7,13 @@
 typedef double coordinate_type;
 typedef boost::geometry::model::d2::point_xy<coordinate_type> point;
 
-struct Vector2D{
+inline double calculate_distance(double x1, double y1, double x2, double y2)
+{
+    return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
+}
+
+typedef struct Vector2D
+{
     double start_x;
     double start_y;
     double end_x;
@@ -18,11 +24,24 @@ struct Vector2D{
     double normalized_x;
     double normalized_y;
 
-    Vector2D(double start_x, double start_y, double end_x, double end_y);
-    point end_point_extended_vector(double new_length);
+    Vector2D(double start_x, double start_y, double end_x, double end_y) : start_x(start_x), start_y(start_y), end_x(end_x), end_y(end_y)
+    {
+        length = calculate_distance(start_x, start_y, end_x, end_y);
+        dx = end_x - start_x;
+        dy = end_y - start_y;
+        normalized_x = dx / length;
+        normalized_y = dy / length;
+    }
 
-};
+    point end_point_extended_vector(double new_length)
+    {
+        double x, y;
+        x = start_x + new_length * normalized_x;
+        y = start_y + new_length * normalized_y;
+        return point(x, y);
+    }
+} Vector2D;
 
-double calculate_distance(double x1, double y1, double x2, double y2);
+
 
 #endif
