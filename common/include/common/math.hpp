5c4f5308b9a96dc54f9b9fda9fa11ca5b4371d3e
Cykus06 <cypkied@gmail.com>
2024-12-24 14:27:01 +0100

    Moved rest of inlines to cpp files

--- common/include/common/math.hpp
+++ common/include/common/math.hpp
@@ -9,10 +9,7 @@ typedef boost::geometry::model::d2::point_xy<coordinate_type> point;
 
 namespace pgr::math
 {
-    inline double calculate_distance(const double x1, const double y1, const double x2, const double y2)
-    {
-        return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
-    }
+    inline double calculate_distance(const double x1, const double y1, const double x2, const double y2);
 
     struct Vector2D
     {
@@ -26,19 +23,9 @@ namespace pgr::math
         double normalized_x;
         double normalized_y;
 
-        Vector2D(const double start_x, const double start_y, const double end_x, const double end_y) : start_x(start_x), start_y(start_y), end_x(end_x), end_y(end_y)
-        {
-            length = calculate_distance(start_x, start_y, end_x, end_y);
-            dx = end_x - start_x;
-            dy = end_y - start_y;
-            normalized_x = dx / length;
-            normalized_y = dy / length;
-        }
+        Vector2D(const double start_x, const double start_y, const double end_x, const double end_y);
 
-        inline point end_point_extended_vector(const double new_length)
-        {
-            return point(start_x + new_length * normalized_x, start_y + new_length * normalized_y);
-        }
+        inline point end_point_extended_vector(const double new_length);
     };
 };
 
--- common/src/cones.cpp
+++ common/src/cones.cpp
@@ -139,4 +139,46 @@ namespace pgr::cones
     {
         m_cone_pairs = std::move(rhs.m_cone_pairs);
     }
+
+    inline void separate_cone_sides(const std::vector<pgr::cones::Cone> &input_cone_array, pgr::cones::ConeArray &inner_cone_array, pgr::cones::ConeArray &outer_cone_array)
+    {
+        for (auto &item : input_cone_array)
+        {
+            if (item.get_side() == pgr::cones::Cone::TrackSide::OUTER)
+            {
+                outer_cone_array.add_cone(item);
+            }
+            else if (item.get_side() == pgr::cones::Cone::TrackSide::INNER)
+            {
+                inner_cone_array.add_cone(item);
+            }
+        }
+    }
+
+    inline void separate_cone_sides_from_cone_pairs(const pgr::cones::ConePairArray &cone_pair_array, pgr::cones::ConeArray &inner_cone_array, pgr::cones::ConeArray &outer_cone_array)
+    {
+        for (auto &item : cone_pair_array.get_pairs())
+        {
+            inner_cone_array.add_cone(item.cone_inner);
+            outer_cone_array.add_cone(item.cone_outer);
+        }
+    }
+
+    inline bool check_pairs_equality(const pgr::cones::ConePair &pair1, const pgr::cones::ConePair &pair2)
+    {
+        bool inner_cone_equality = false;
+        bool outer_cone_equality = false;
+
+        if ((pair1.cone_inner.get_x() == pair2.cone_inner.get_x()) && (pair1.cone_inner.get_y() == pair2.cone_inner.get_y()))
+        {
+            inner_cone_equality = true;
+        }
+
+        if ((pair1.cone_outer.get_x() == pair2.cone_outer.get_x()) && (pair1.cone_outer.get_y() == pair2.cone_outer.get_y()))
+        {
+            outer_cone_equality = true;
+        }
+
+        return inner_cone_equality && outer_cone_equality;
+    }
 };
