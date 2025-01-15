757fd35f9765c88653de666aae73565263b2ecf7
bovivian <vadzim.shy@gmail.com>
2025-01-15 21:11:11 +0100

    remove inline and dublicate create msg functions

--- common/include/common/msg.hpp
+++ common/include/common/msg.hpp
@@ -7,7 +7,7 @@
 #include <geometry_msgs/msg/vector3.hpp>
 
 /**
- * `inline` function. Creates `geometry_msgs` `Point` with given `x`,
+ * Creates `geometry_msgs` `Point` with given `x`,
  * `y` and `z`.
  * @param x double x-axis value.
  * @param y double y-axis value.
@@ -15,25 +15,25 @@
  * @return `geometry_msgs::msg::Point` point with given `x`, `y` and
  * `z`.
  */
-inline geometry_msgs::msg::Point create_point(
+geometry_msgs::msg::Point create_point(
     const double x,
     const double y,
     const double z);
 
 /**
- * `inline` function. Creates `geometry_msgs` `Pose` with given
+ * Creates `geometry_msgs` `Pose` with given
  * position and orientation.
  * @param point `geometry_msgs::msg::Point` position.
  * @param quaternion `geometry_msgs::msg::Quaternion` orientation.
  * @return `geometry_msgs::msg::Pose` pose with given position and
  * orientation.
  */
-inline geometry_msgs::msg::Pose create_pose(
+geometry_msgs::msg::Pose create_pose(
     const geometry_msgs::msg::Point point,
     const geometry_msgs::msg::Quaternion quaternion);
 
 /**
- * `inline` function. Creates `geometry_msgs` `Quaternion` with
+ * Creates `geometry_msgs` `Quaternion` with
  * given `x`, `y`, `z` and `w`.
  * @param x double x of vector of imaginary part.
  * @param y double y of vector of imaginary part.
@@ -42,14 +42,14 @@ inline geometry_msgs::msg::Pose create_pose(
  * @return `geometry_msgs::msg::Quaternion` quaternion with given
  * `x`, `y`, `z` and `w`.
  */
-inline geometry_msgs::msg::Quaternion create_quaternion(
+geometry_msgs::msg::Quaternion create_quaternion(
     const double x,
     const double y,
     const double z,
     const double w);
 
 /**
- * `inline` function. Creates `geometry_msgs` `Vector3` with given
+ * Creates `geometry_msgs` `Vector3` with given
  * `x`, `y` and `z`.
  * @param x double x value.
  * @param y double y value.
@@ -57,7 +57,7 @@ inline geometry_msgs::msg::Quaternion create_quaternion(
  * @return `geometry_msgs::msg::Vector3` vector with given `x`, `y`
  * and `z`.
  */
-inline geometry_msgs::msg::Vector3 create_vector3(
+geometry_msgs::msg::Vector3 create_vector3(
     const double x,
     const double y,
     const double z);
--- common/src/msg.cpp
+++ common/src/msg.cpp
@@ -1,15 +1,6 @@
 #include "common/msg.hpp"
 
-/**
- * `inline` function. Creates `geometry_msgs` `Point` with given `x`,
- * `y` and `z`.
- * @param x double x-axis value.
- * @param y double y-axis value.
- * @param z double z-axis value.
- * @return `geometry_msgs::msg::Point` point with given `x`, `y` and
- * `z`.
- */
-inline geometry_msgs::msg::Point create_point(
+geometry_msgs::msg::Point create_point(
     const double x,
     const double y,
     const double z)
@@ -21,15 +12,7 @@ inline geometry_msgs::msg::Point create_point(
     return point;
 }
 
-/**
- * `inline` function. Creates `geometry_msgs` `Pose` with given
- * position and orientation.
- * @param point `geometry_msgs::msg::Point` position.
- * @param quaternion `geometry_msgs::msg::Quaternion` orientation.
- * @return `geometry_msgs::msg::Pose` pose with given position and
- * orientation.
- */
-inline geometry_msgs::msg::Pose create_pose(
+geometry_msgs::msg::Pose create_pose(
     const geometry_msgs::msg::Point point,
     const geometry_msgs::msg::Quaternion quaternion)
 {
@@ -39,17 +22,7 @@ inline geometry_msgs::msg::Pose create_pose(
     return pose;
 }
 
-/**
- * `inline` function. Creates `geometry_msgs` `Quaternion` with
- * given `x`, `y`, `z` and `w`.
- * @param x double x of vector of imaginary part.
- * @param y double y of vector of imaginary part.
- * @param z double z of vector of imaginary part.
- * @param w double w scalar of real part.
- * @return `geometry_msgs::msg::Quaternion` quaternion with given
- * `x`, `y`, `z` and `w`.
- */
-inline geometry_msgs::msg::Quaternion create_quaternion(
+geometry_msgs::msg::Quaternion create_quaternion(
     const double x,
     const double y,
     const double z,
@@ -63,16 +36,7 @@ inline geometry_msgs::msg::Quaternion create_quaternion(
     return quaternion;
 }
 
-/**
- * `inline` function. Creates `geometry_msgs` `Vector3` with given
- * `x`, `y` and `z`.
- * @param x double x value.
- * @param y double y value.
- * @param z double z value.
- * @return `geometry_msgs::msg::Vector3` vector with given `x`, `y`
- * and `z`.
- */
-inline geometry_msgs::msg::Vector3 create_vector3(
+geometry_msgs::msg::Vector3 create_vector3(
     const double x,
     const double y,
     const double z)
